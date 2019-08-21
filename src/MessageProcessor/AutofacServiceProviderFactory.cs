aae84f2c52fb385f0c0340ee75bbb4b3457a1b0d
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2019-08-21 22:58:06 +0100

    Console project with generic host

---
+++ src/MessageProcessor/AutofacServiceProviderFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Autofac;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MessageProcessor
+{
+    public class AutofacServiceProviderFactory : IServiceProviderFactory<ContainerBuilder>
+    {
+        public ContainerBuilder CreateBuilder(IServiceCollection services)
+        {
+            return new ContainerBuilder();
+        }
+
+        public IServiceProvider CreateServiceProvider(ContainerBuilder containerBuilder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

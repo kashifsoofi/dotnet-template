f25a0f9b87a2bf1388a21c9fa4a9b03df99bf4cb
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2021-05-07 10:11:38 +0100

    Add option to add message handler in api project

---
+++ templates/webapi-service/src/Template.Api/Handlers/CreateAggregateNameCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿namespace Template.Api.Handlers
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using Template.Contracts.Messages.Commands;
+    using Template.Domain.Aggregates.AggregateName;
+
+    public class CreateAggregateNameCommandHandler : IHandleMessages<CreateAggregateName>
+    {
+        private readonly IAggregateNameAggregateReadRepository aggregateReadRepository;
+        private readonly IAggregateNameAggregateWriteRepository aggregateWriteRepository;
+
+        public CreateAggregateNameCommandHandler(IAggregateNameAggregateReadRepository aggregateReadRepository, IAggregateNameAggregateWriteRepository aggregateWriteRepository)
+        {
+            this.aggregateReadRepository = aggregateReadRepository;
+            this.aggregateWriteRepository = aggregateWriteRepository;
+        }
+
+        public async Task Handle(CreateAggregateName command, IMessageHandlerContext context)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            if (command.Id == Guid.Empty)
+            {
+                throw new ArgumentException("Guid value cannot be default", nameof(command.Id));
+            }
+
+            try
+            {
+                var aggregate = await this.aggregateReadRepository.GetByIdAsync(command.Id);
+                aggregate.Create(command);
+
+                await PersistAndPublishAsync(aggregate, context);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private async Task PersistAndPublishAsync(IAggregateNameAggregate aggregate, IMessageHandlerContext context)
+        {
+            if (aggregate.IsNew)
+            {
+                await aggregateWriteRepository.CreateAsync(aggregate);
+            }
+            else
+            {
+                await aggregateWriteRepository.UpdateAsync(aggregate);
+            }
+
+            await Task.WhenAll(aggregate.UncommittedEvents.Select(async (x) => await context.Publish(x)));
+        }
+    }
+}
---
+++ templates/webapi-service/src/Template.Api/Handlers/DeleteAggregateNameCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿namespace Template.Api.Handlers
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using Template.Contracts.Messages.Commands;
+    using Template.Domain.Aggregates.AggregateName;
+
+    public class DeleteAggregateNameCommandHandler : IHandleMessages<DeleteAggregateName>
+    {
+        private readonly IAggregateNameAggregateReadRepository aggregateReadRepository;
+        private readonly IAggregateNameAggregateWriteRepository aggregateWriteRepository;
+
+        public DeleteAggregateNameCommandHandler(IAggregateNameAggregateReadRepository aggregateReadRepository, IAggregateNameAggregateWriteRepository aggregateWriteRepository)
+        {
+            this.aggregateReadRepository = aggregateReadRepository;
+            this.aggregateWriteRepository = aggregateWriteRepository;
+        }
+
+        public async Task Handle(DeleteAggregateName command, IMessageHandlerContext context)
+        {
+            if (command == null || command.Id == Guid.Empty)
+            {
+                throw new ArgumentException(nameof(command));
+            }
+
+            try
+            {
+                var aggregate = await this.aggregateReadRepository.GetByIdAsync(command.Id);
+                aggregate.Delete();
+
+                await PersistAndPublishAsync(aggregate, context);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private async Task PersistAndPublishAsync(IAggregateNameAggregate aggregate, IMessageHandlerContext context)
+        {
+            await aggregateWriteRepository.DeleteAsync(aggregate);
+
+            await Task.WhenAll(aggregate.UncommittedEvents.Select(async (x) => await context.Publish(x)));
+        }
+    }
+}

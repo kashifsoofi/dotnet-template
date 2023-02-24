3265f80f8d6fd8e84712d60096e69f22326add23
Kashif Soofi <kashif.soofi+git@gmail.com>
2023-02-24 23:49:47 +0000

    Add region for bucket

--- templates/webapi-service/localstack-setup/setup-localstack.sh
+++ templates/webapi-service/localstack-setup/setup-localstack.sh
@@ -13,4 +13,4 @@ aws --region eu-west-1 --endpoint-url=http://localstack:4566 sqs create-queue --
 aws --region eu-west-1 --endpoint-url=http://localstack:4566 sqs create-queue --queue-name template-host
 aws --region eu-west-1 --endpoint-url=http://localstack:4566 sqs create-queue --queue-name error
 aws --region eu-west-1 --endpoint-url=http://localstack:4566 s3api create-bucket --bucket bucketname
-aws --endpoint-url=http://localstack:4566 s3api put-bucket-acl --bucket bucketname --acl public-read
+aws --region eu-west-1 --endpoint-url=http://localstack:4566 s3api put-bucket-acl --bucket bucketname --acl public-read
